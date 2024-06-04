bb4b9ae41ea90d3b17834142170da532e9eaa00e
BROWb <127416062+BROWb@users.noreply.github.com>
2024-06-04 09:03:36 -0300

    mudança
    inici de tudo

---
+++ componentes.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>componentes</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/style.css"
+</head>
+<body>
+    <section class="container">
+        <nav class="navbar">
+        <ul>
+                <li><a href="/syzs/index.php">HOME</a></li>
+                <li><a href="/partes-sergio-syzs/componentes.php">COMPONENTES</a>
+                </li>
+                <li><a href="/partes-sergio-syzs/quemsomos.php">QUEM SOMOS</a></li>
+                <li>
+                <a href="#">MANUTENÇÕES</a>
+                  <ul class="dropdown">
+                    <li><a href="placa-mae.html">PLACA-MÃE</a></li>
+                    <li><a href="cpu.html">CPU</a></li>
+                    <li><a href="memoria-ram.html">MEMÓRIA RAM</a></li>
+                    <li><a href="disco-rigido.html">DISCO RÍGIDO</a></li>
+                    <li><a href="ssd.html">SSD</a></li>
+                    <li><a href="gpu.html">GPU</a></li>
+                    <li><a href="psu.html">PSU</a></li>
+                  </ul>
+                </li>
+            </ul>
+        </nav>
+
+<article>
+
+<div>
+        <font color="#4B0082">
+            <h2>O que é placa-mãe  </h2>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+            </font>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h4>A placa-mãe é a base do computador. Ela contém a CPU e é uma espécie de central por onde todos os outros componentes de hardware passam. A placa-mãe age como o cérebro do computador, alocando potência onde necessário e comunicando-se com todos os componentes e coordenando-os, o que a torna uma das peças de hardware mais importantes de um computador.
+                 Ao escolher uma placa-mãe, é importante checar que portas de hardware ela oferece. É vital ver o nível das portas USB oferecidas (USB 2.0, 3.0, 3.1), além dos tipos de porta de tela usados (HDMI, DVI, RGB) e a quantidade total de cada uma. As portas da placa-mãe também ajudam a entender quais outros componentes de hardware serão compatíveis com seu computador, como o tipo de RAM e de placa de vídeo que você pode usar.
+                Embora a placa-mãe seja só uma peça do circuito inteiro, é nela que uma das peças de hardware mais importantes: o processador.
+                </h2>
+                       
+                </article>
+                </td>
+                <td>
+                <div class="img1">
+                <img src="placam.png">
+                <p>Fonte: Google Fotos</p>
+
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+
+        <div>
+        <font color="#4B0082">
+            <h2>O que é uma CPU (processador/unidade de processamento central)</h2>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+            </font>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h4>CPU é a sigla para Central Process Unit, ou Unidade Central de Processamento. Ele é o principal item de Hardware do computador, que também é conhecido como processador. A CPU é responsável por calcular e realizar tarefas determinadas pelo usuário e é considerado o cérebro do PC.
+Uma CPU é feita de bilhões de transistores, que são pequenas peças microscópicas que controlam a corrente elétrica do componente.
+                </h2>
+                </article>
+                </td>
+                <td>
+                <div class="img3">
+                <img src="cpu.png" width="500px" ; >
+                <p>Fonte: Google Fotos</p>
+
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+        <div>
+        <font color="#4B0082">
+            <h2>O que é memoria RAM?</h2>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+            </font>
+        </div>
+        <table>
+            <tr>
+                <td>
+        <article class="ram">
+                <br>
+                <h4>A memória de acesso aleatório, ou RAM, é o hardware encontrado nas entradas de memória da placa-mãe. O papel da RAM é armazenar em caráter instantâneo e temporário as informações criadas por programas e fazer isso de forma que os dados possam ser acessados depois imediatamente. Algumas das tarefas que exigem memória aleatória, ou RAM, são: renderizar imagens para design gráfico, vídeos ou fotografias editadas, trabalhar em diversos aplicativos ao mesmo tempo (por exemplo, ter um jogo aberto em uma tela e conversar no Discord em outra).
+                </h2>
+                </article>
+                </td>
+              <td>
+                <div class="ram">
+                <img src="ram.png" width="500px" ;
+                <p>Fonte: Google Fotos</p>
+  
+                
+                </div>
+                </td>
+                </tr>
+                </table>
+            
+        
+
+
+        <div>
+        <font color="#4B0082">
+            <h2>O que é um disco rígido?</h2>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+            </font>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h4>O disco rígido é o dispositivo de armazenamento responsável por armazenar dados permanentes e temporários. Esses dados vêm de muitas formas diferentes, mas, a grosso modo, tudo é salvo ou instalado no computador: por exemplo, programas de computador, fotos da família, o sistema operacional, documentos de processamento de texto e muitos outros.
+
+                </article>
+                </td>
+                <td>
+                <div class="img5">
+                <img src="hd.png" width="500px" ; >
+                <p>Fonte: Google Fotos</p>
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+
+        <div>
+        <font color="#4B0082">
+            <h2>O que é um ssd?</h2>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+            </font>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h4> é um componente que armazena dados em computadores e consoles de videogame. Ele substitui o HDD (Hard Disk Drive) tradicional e ainda garante mais rapidez ao sistema, graças às velocidades maiores de leitura e gravação.
+                </article>
+                </td>
+                <td>
+                <div class="img6" ;>
+                <img src="ssd.png"width="500px" >
+                <p>Fonte: Google Fotos</p>
+
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+
+        <div>
+        <font color="#4B0082">
+            <h2>O que é uma unidade de processamento gráfico (GPU)?</h2>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+            </font>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h4> Importante principalmente para renderizações 3D, a GPU faz exatamente o que seu nome sugere: processa blocos enormes de dados gráficos. Você verá que a placa de vídeo do seu computador tem pelo menos uma GPU. Diferentemente dos recursos gráficos integrados básicos que as placas-mãe oferecem, as placas de vídeo dedicadas se conectam à placa-mãe por meio de uma entrada de expansão para trabalhar quase que exclusivamente na renderização gráfica. Isso também significa que você pode trocar sua placa de vídeo se quiser melhorar um pouco o desempenho do seu computador.
+E não é só isso: as GPUs modernas cumprem um papel computacional mais amplo que não se limita a renderizar, o que as torna uma extensão da unidade de processamento central, ou CPU.
+
+                </article>
+                </td>
+                <td>
+                <div class="img7 " ;>
+                <img src="und.png"width="500px" >
+                <p>Fonte: Google Fotos</p>
+
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+
+        <div>
+        <font color="#4B0082">
+            <h2>O que é uma unidade de alimentação (PSU)?</h2>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+            </font>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h4> A fonte de alimentação é um componente de aparelhos eletrônicos que serve para transformar a energia elétrica que chega pelas tomadas em uma corrente elétrica contínua, ou seja, a fonte de alimentação recebe a energia em 110V ou 220V e transforma na voltagem adequada para o funcionamento do aparelho, que geralmente é de 12V ou 24V.
+
+Uma fonte de alimentação pode elevar ou baixar o nível de tensão, e também há aquelas que apenas isolam o circuito da rede de energia elétrica. A fonte de alimentação ainda é um importante protetor contra picos de energia e instabilidade, muito comuns no Brasil, sendo que cada tipo de equipamento exigirá uma fonte de alimentação específica.
+                </article>
+                </td>
+                <td>
+                <div class="img8">
+                <img src="fonte.png" >
+                <p> Fonte: Google Fotos</p>
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+
+
+    </section>
+
+  <footer>
+    <div id="footer_content">
+        <div id="footer_contacts">
+            <h1>Logo</h1>
+            <p>It's all about your dreams.</p>
+
+            <div id="footer_social_media">
+                <a href="#" class="footer-link" id="instagram">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+
+                <a href="#" class="footer-link" id="facebook">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </a>
+
+                <a href="#" class="footer-link" id="whatsapp">
+                    <i class="fa-brands fa-whatsapp"></i>
+                </a>
+            </div>
+        </div>
+        
+        <ul class="footer-list">
+            <li>
+                <h3>home</h3>
+            </li>
+            <li>
+                <a href="#" class="footer-link">Componentes</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">Manutenção</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">Quem somos?</a>
+            </li>
+        </ul>
+
+        <ul class="footer-list">
+            <li>
+                <h3>Products</h3>
+            </li>
+            <li>
+                <a href="#" class="footer-link">App</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">Desktop</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">Cloud</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+
+    <div id="footer_copyright">
+        &#169
+        2023 all rights reserved
+    </div>
+</footer>
+
+    
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/carro.css">
+
+    <title>home</title>
+</head>
+<body>
+
+
+<section class="container">
+        <nav class="navbar">
+            <ul>
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="componentes.php">COMPONENTES</a>
+                </li>
+                <li><a href="quemsomos.php">QUEM SOMOS</a></li>
+                <li>
+                <a href="#">MANUTENÇÕES</a>
+                  <ul class="dropdown">
+                    <li><a href="./manutençao/placa-mae.html">PLACA-MÃE</a></li>
+                    <li><a href="./manutençao/cpu.html">CPU</a></li>
+                    <li><a href="./manutençao/memoria-ram.html">MEMÓRIA RAM</a></li>
+                    <li><a href="./manutençao/disco-rigido.html">DISCO RÍGIDO</a></li>
+                    <li><a href="./manutençao/ssd.html">SSD</a></li>
+                    <li><a href="./manutençao/gpu.html">GPU</a></li>
+                    <li><a href="./manutençao/psu.html">PSU</a></li>
+                    
+                  </ul>
+                </li>
+            </ul>
+          </nav>
+          
+          <div class="slideshow">
+    <a href="seu-link1">
+        <img src="ddr2.jpg" alt="Imagem 1">
+    </a>
+    <a href="seu-link2">
+        <img src="alcool.jpg" alt="Imagem 2">
+    </a>
+    <a href="seu-link3">
+        <img src="vdd.png" alt="Imagem 3">
+    </a>
+</div>
+
+<article>
+
+        <div>
+          <font color="#4B0082">
+            <h2>O que é o hardware:</h2>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+          </font>
+        </div>
+
+           <h3>Hardware é qualquer elemento físico de computadores e outros sistemas microeletrônicos. Ele pode ser um dispositivo externo ou interno. Em inglês, hardware significa “equipamento”.
+           </h3>
+           <br>
+
+        <div>
+          <font color="#4B0082">
+            <h2>Hardware interno:</h2>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+          </font>
+        </div>
+           <h3>
+           Se refere aos componentes localizados no interior do dispositivo, essenciais para o seu funcionamento. Ou seja, sem um deles, o celular, tablet ou PC não funciona corretamente ou, simplesmente, é inutilizado.
+          <br><br>
+            Exemplos: placas de memória RAM, discos rígidos, SSDs internos, leitores de mídia (CD, DVD ou Blue Ray), placas-mãe, processadores, coolers, fontes, placas de vídeo, etc.
+            </h3>
+          <br>
+          <div>
+          <font color="#4B0082">
+            <h2>Hardware externo:</h2>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+          </font>
+        </div>
+            <h3>São todos os componentes conectados externamente a um dispositivo, por portas como USB, VGA, Ethernet, HDMI, de áudio, etc. Em geral, agregam funções não essenciais ao computador, como imprimir um documento, ouvir áudio ou ampliar a capacidade de armazenamento. São, em suma, todos os periféricos conectados.
+            <br><br>
+            Exemplos: impressoras, scanners, monitores, mouses, teclados, webcams, fones de ouvido, microfones, auto-falantes, cabos, unidades de armazenamento externas (HDs, cartões SSD, pendrives), projetores, joysticks etc.</h3>
+          
+
+
+        </article>
+
+
+    <footer>
+        <div id="footer_content">
+            <div id="footer_contacts">
+                <h1>Logo</h1>
+                <p>Saiba mais sobre:</p>
+
+                <div id="footer_social_media">
+                    <a href="#" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="facebook">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="whatsapp">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                </div>
+            </div>
+            
+            <ul class="footer-list">
+                <li>
+                    <h3>home</h3>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Componentes</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Manutenção</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Quem somos?</a>
+                </li>
+            </ul>
+
+            <ul class="footer-list">
+                <li>
+                    <h3>Products</h3>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">App</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Desktop</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Cloud</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+
+        <div id="footer_copyright">
+            &#169
+            2024 direitos reservados a Syzs Tech
+        </div>
+    </footer>
+</body>
+</html>
